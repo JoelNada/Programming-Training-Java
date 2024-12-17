4ff15dd1794350c0c53cc47a3e6588c45b1a0097
joelnada789 <joelnada789@gmail.com>
2024-12-17 18:09:29 +0530

    few changes

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -9,7 +9,6 @@ public class DecimalToBinary {
         if(power==1)
             return 2;
         else return num*power(num,power-1);
-
     }
 
     private static String toBinary(int number) {
@@ -19,18 +18,24 @@ public class DecimalToBinary {
             System.out.println(number);
         }
         StringBuilder binary = new StringBuilder();
-        while(number!=0){ int remainder = 0;
+        while(number!=0)
+        { int remainder = 0;
             remainder=number%2;
             binary.append(remainder);
-            number/=2;}
+            number/=2;
+        }
        return binary.reverse().toString();
-
 }
     private static int toDecimal(String binary) {
         int length = binary.length(),res=0;
         System.out.println("length : "+length);
         StringBuilder bin = new StringBuilder(binary).reverse();
-      int num = Integer.parseInt(Arrays.stream(bin.toString().split("")).map(s->(Character)s.charAt(0)).filter(Character::isDigit).map(s -> String.valueOf((char) s)).collect(Collectors.joining()));
+      int num = Integer.parseInt(Arrays
+              .stream(bin.toString().split(""))
+              .map(s->(Character)s.charAt(0))
+              .filter(Character::isDigit)
+              .map(s -> String.valueOf((char) s))
+              .collect(Collectors.joining()));
       while(num!=0){
           res=res+(num%10*power(2,length-1));
           length-=1;num=num/10;
@@ -38,11 +43,11 @@ public class DecimalToBinary {
         return res;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int number = in.nextInt();
-
         String binaryNumber = toBinary(number);
         System.out.printf("The binary format of %s is %s\n",number,binaryNumber);
         System.out.print("Do you want to convert it back to decimal form ? (Y/N) : ");
@@ -55,8 +60,4 @@ public class DecimalToBinary {
             System.out.println("Have a nice day, See you later !!");
         }
     }
-
-
-
-
 }
